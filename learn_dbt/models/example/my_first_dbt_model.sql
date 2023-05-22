8a5c70e2ed8bd01dd2bb39f32501c91d1d95db99
Alba Nemeth-Buhin <buhina@starschema.com>
2023-05-22 10:09:07 +0200

    add snapshot configuration

--- learn_dbt/models/example/my_first_dbt_model.sql
+++ learn_dbt/models/example/my_first_dbt_model.sql
@@ -7,15 +7,15 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table', alias='my_first_model') }}
+{{ config(materialized='table', alias='my_first_model', post_hook='grant select on {{this}} to role analyst') }}
 
 with source_data as (
 
-    select 1 as id
+    select 1 as id, 'CA' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
     union all
-    select null as id
+    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
     union all
-    select 3 as id
+    select 3 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
 
 )
 
